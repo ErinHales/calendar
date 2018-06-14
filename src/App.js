84ccdae4fa6718dd3697f0d8b07931e28e275eea
nathan-08 <nathanryan001@gmail.com>
2018-06-14 17:11:23 -0600

    month selector

--- src/App.js
+++ src/App.js
@@ -6,7 +6,9 @@ import Month from "./components/Month/Month";
 class App extends Component {
   constructor() {
     super();
-    this.state = {};
+    this.state = {
+      monthNum: 0
+    };
   }
   render() {
     const dayNames = ["sunday", "monday", "tuesday", "wednesday", "thursday", "friday", "saturday"];
@@ -20,20 +22,37 @@ class App extends Component {
       "july",
       "august",
       "september",
+      "october",
       "november",
       "december"
     ];
-    const date = new Date();
+
+    const monthNum = Number(this.state.monthNum); // set month here (0 indexed)
+    const year = new Date().getFullYear();
+    const date = new Date(year, monthNum);
     const numDaysInMonth = date.monthDays();
-    const monthNum = 8; // set month here (0 indexed)
-    const year = date.getFullYear();
     const startDay = new Date(year, monthNum, 1).getDay();
     let days = [];
     for (let i = 0; i < numDaysInMonth; i++) {
       days.push(<Month dayNum={i + 1} day={dayNames[(i + startDay) % 7]} />);
     }
+    console.log(this.state.monthNum);
     return (
       <div>
+        <select className="month-select" onChange={e => this.setState({ monthNum: e.target.value })}>
+          <option value={0}>January</option>
+          <option value={1}>February</option>
+          <option value={2}>March</option>
+          <option value={3}>April</option>
+          <option value={4}>May</option>
+          <option value={5}>June</option>
+          <option value={6}>July</option>
+          <option value={7}>August</option>
+          <option value={8}>September</option>
+          <option value={9}>October</option>
+          <option value={10}>November</option>
+          <option value={11}>December</option>
+        </select>
         <h2 className="month-title">{`${monthNames[monthNum]} ${year}`}</h2>
         <div className="grid-container">{days}</div>
       </div>
